c188afe9ca5a110a1b7e167f489d0bacd6522426
Lixerech <Lixerech@gmail.com>
2023-02-18 12:49:37 +0200

    Create Project

---
+++ HorrorTime/Source/HorrorTime/HorrorTimeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HorrorTimeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORRORTIME_API AHorrorTimeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
